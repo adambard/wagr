3849097d3681544610ed406bb4c95f4cf2baebbb
Adam Bard <adam@adambard.com>
2013-03-13 04:10:05 -0700

    Keep more timing information

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [http-kit "2.0.0-RC4"]
                  [schemongo "0.1.2"]
+                 [com.rubiconproject.oss/jchronic "0.2.6"]
                  [congomongo "0.1.10"]
                  [enlive "1.0.1"]
                  [compojure "1.1.3"]]
--- src/wagr/data/wagers.clj
+++ src/wagr/data/wagers.clj
@@ -1,6 +1,7 @@
 (ns wagr.data.wagers
   (:require [schemongo.models :as models]
             [somnium.congomongo :as mongo]
+            [wagr.helpers :as helpers]
             ))
 
 (def wager-schema
@@ -10,11 +11,22 @@
    [:bet :str]
    [:wager :str]
    [:date :str]
+   [:timestamp :int] ; The timestamp version of the date input
+   [:created :int]
    [:email1 :str]
    [:email2 :str]
    ])
 
-(models/model "wager" :wagers wager-schema)
+(models/model "wager-" :wagers wager-schema)
+
+(defn create-wager! [data]
+  (let [timestamp (helpers/parse-date (:date data))
+        timedata {
+                  :timestamp timestamp
+                  :created (helpers/now)
+                  }
+        ]
+    (create-wager-! (merge data timedata))))
 
 (defn top-wagers [n]
   (mongo/fetch :wagers :sort {:_id -1} :limit n))
