a7d685da0d4420dd976aa00e89de4940299a0115
Adam Bard <github@adambard.com>
2013-03-13 20:28:20 +0000

    Added helpers

---
+++ src/wagr/helpers.clj
@@ -0,0 +1,13 @@
+(ns wagr.helpers
+  (:import
+    java.util.Date
+	com.mdimension.jchronic.Chronic))
+
+(defn parse-date [s]
+  (-> (Chronic/parse s)
+    .getBeginCalendar
+	.getTime
+	.getTime))
+
+(defn now []
+  (.getTime (Date.)))
